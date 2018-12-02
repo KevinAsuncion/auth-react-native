25cdf28e66eb6c8b9c50d0b46a329ff894baae4c
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-02 09:18:06 -0800

    add spinner and input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
-import {View,Text} from 'react-native';
-import { Header } from './components/common';
+import React, { Component } from "react";
+import { View } from "react-native";
+import firebase from "firebase";
+import { Header } from "./components/common";
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-    render() {
-        return (
-            <View>
-                <Header headerText="Authentication" />
-                <Text>This is the app</Text>
-            </View>
-        );
-    }
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAZ8xNwotb9zGNM7umnS_c6ykVd8-Ee02Y",
+      authDomain: "auth-21af9.firebaseapp.com",
+      databaseURL: "https://auth-21af9.firebaseio.com",
+      projectId: "auth-21af9",
+      storageBucket: "auth-21af9.appspot.com",
+      messagingSenderId: "726976649194"
+    });
+  }
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <LoginForm />
+      </View>
+    );
+  }
 }
 
 export default App;
